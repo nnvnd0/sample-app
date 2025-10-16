bf8eab6b7537ad11e02b5b79db50dc92955939cb
Fernando Pena <fernandogps34@gmail.com>
2025-10-16 19:02:59 -0400

    chore: init Node app with Express

---
+++ index.js
@@ -0,0 +1,8 @@
+﻿const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Hello from Git + GitHub + Heroku!"));
+app.get("/about", (req, res) => res.send("About: demo route."));
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
